ece7724457d9981379d70ca9f2ea329b8ea1f2a1
alcexhim <alcexhim@gmail.com>
2015-03-06 00:05:23 -0500

    Redirect Admin pages to login page if required

--- Tenant/Include/MasterPages/Admin.php
+++ Tenant/Include/MasterPages/Admin.php
@@ -1,8 +1,20 @@
 <?php
 	namespace FamilySuite\Tenant\MasterPages;
-	
-	class Admin
+
+	use WebFX\CancelEventArgs;
+	use WebFX\Parser\WebFXPage;
+	use WebFX\System;
+			
+	class Admin extends WebFXPage
 	{
+		public function OnInitializing($e)
+		{
+			if ($_SESSION["Authentication.UserName"] == null)
+			{
+				$e->Cancel = true;
+				System::Redirect("~/account/login");
+			}
+		}
 		
 	}
 ?>
