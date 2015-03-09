5c6a96532f9a3e930f8a2a91cfff725dd3d7b5b2
alcexhim <alcexhim@gmail.com>
2015-03-09 09:29:32 -0400

    Additional enhancements including page title and footer loaded from configuration file

--- Tenant/Include/Modules/001-Admin/Pages/LoginPage.phpx.php
+++ Tenant/Include/Modules/001-Admin/Pages/LoginPage.phpx.php
@@ -1,10 +1,15 @@
 <?php
-	namespace FamilySuite\Tenant\Modules\Frontend\Pages;
+	namespace FamilySuite\Tenant\Modules\Admin\Pages;
 	
 	use Phast\Parser\PhastPage;
 	use Phast\EventArgs;
-		
+	use Phast\System;
+				
 	class LoginPage extends PhastPage
 	{
+		public function OnClassLoaded(EventArgs $e)
+		{
+			$this->Page->GetControlByID("paraFooter")->Content = System::GetConfigurationValue("Pages.LoginPage.FooterContent");
+		}
 	}
 ?>
