24779e934c2441a3ca6bd83d1884281407bf9047
alcexhim <alcexhim@gmail.com>
2015-03-30 23:55:31 -0400

    Major improvements to Event page to actually get it to do something

--- Tenant/Include/Pages/Event.phpx.php
+++ Tenant/Include/Pages/Event.phpx.php
@@ -4,6 +4,9 @@
 	use Phast\Parser\PhastPage;
 	use Phast\CancelEventArgs;
 	
+	use Phast\Data\DataSystem;
+	use Phast\System;
+	
 	class EventPage extends PhastPage
 	{
 		public function OnInitializing(CancelEventArgs $e)
@@ -12,14 +15,48 @@
 			
 			if ($_SERVER["REQUEST_METHOD"] == "POST")
 			{
-				$page->GetControlByID("wizard")->SelectedPageID = "page2";
+				$page1 = $page->GetControlByID("wizard")->GetPageByID("page1");
+				$formview1 = $page1->GetControlByID("formview1");
+				
+				$formview1->GetItemByID("txtName")->Value = $_POST["resp_EnteredName"];
+				$formview1->GetItemByID("txtEmailAddress")->Value = $_POST["resp_EnteredEmailAddress"];
+				$formview1->GetItemByID("txtMessage")->Value = $_POST["resp_Message"];
+				
+				if ($_POST["resp_Status"] != "-1")
+				{
+					// we're going to actually submit data now
+					$pdo = DataSystem::GetPDO();
+					$query = "INSERT INTO Responses (resp_EnteredName, resp_EnteredEmailAddress, resp_Message, resp_AssociatedNameID, resp_MealOptionID, resp_MealPlanComments, resp_GuestCount, resp_Status) VALUES (" .
+						":resp_EnteredName, :resp_EnteredEmailAddress, :resp_Message, :resp_AssociatedNameID, :resp_MealOptionID, :resp_MealPlanComments, :resp_GuestCount, :resp_Status)";
+					$statement = $pdo->prepare($query);
+					$statement->execute(array
+					(
+						":resp_EnteredName" => $_POST["resp_EnteredName"],
+						":resp_EnteredEmailAddress" => $_POST["resp_EnteredEmailAddress"],
+						":resp_Message" => $_POST["resp_Message"],
+						":resp_AssociatedNameID" => null,
+						":resp_MealOptionID" => $_POST["guest_MealPlanID"],
+						":resp_MealPlanComments" => $_POST["guest_SpecialDietaryNeeds"],
+						":resp_GuestCount" => $_POST["resp_GuestCount"],
+						":resp_Status" => $_POST["resp_Status"]
+					));
+					
+					if ($_POST["resp_Status"] == "1")
+					{
+						System::Redirect("~/events/" . $page->GetPathVariableValue("eventID") . "/thankyou1");
+					}
+					else if ($_POST["resp_Status"] == "0")
+					{
+						System::Redirect("~/events/" . $page->GetPathVariableValue("eventID") . "/thankyou2");
+					}
+					return;
+				}
+				else
+				{
+					// take us directly to page 2 since we came from the rsvp landing page
+					$page->GetControlByID("wizard")->SelectedPageID = "page2";
+				}
 			}
-			
-			$page1 = $page->GetControlByID("wizard")->GetPageByID("page1");
-			$formview1 = $page1->GetControlByID("formview1");
-			$formview1->GetItemByID("txtName")->Value = $_POST["resp_EnteredName"];
-			$formview1->GetItemByID("txtEmailAddress")->Value = $_POST["resp_EnteredEmailAddress"];
-			$formview1->GetItemByID("txtMessage")->Value = $_POST["resp_Message"];
 		}
 	}
 ?>
