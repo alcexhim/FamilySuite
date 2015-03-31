9bc916bdbe6406191e732e8b8c3bfe6012d08c16
alcexhim <alcexhim@gmail.com>
2015-03-31 15:58:48 -0400

    Begin to implement login page with login key (for administrators inviting other users)

--- Tenant/Include/Modules/001-Admin/Pages/LoginPage.phpx.php
+++ Tenant/Include/Modules/001-Admin/Pages/LoginPage.phpx.php
@@ -4,12 +4,25 @@
 	use Phast\Parser\PhastPage;
 	use Phast\EventArgs;
 	use Phast\System;
-				
+	use Phast\CancelEventArgs;
+					
 	class LoginPage extends PhastPage
 	{
 		public function OnClassLoaded(EventArgs $e)
 		{
 			$this->Page->GetControlByID("paraFooter")->Content = System::GetConfigurationValue("Pages.LoginPage.FooterContent");
+		}
+		
+		public function OnInitializing(CancelEventArgs $e)
+		{
+			$page = $e->RenderingPage;
+			
+			$key = $page->GetPathVariableValue("key");
+			if ($key != null)
+			{
+				$user = User::GetByLoginKey($key);
+				
+			}
 		}
 	}
 ?>
